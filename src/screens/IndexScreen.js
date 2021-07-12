3a0b1d70396c5266548d8a13548520a49c90d8c1
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-12 21:01:33 +0300

    feat: Add routing and scaffold for main screen. Add prettier settings

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const IndexScreen = () => (
+  <View>
+    <Text>1Index Screen</Text>
+  </View>
+);
+
+export default IndexScreen;
