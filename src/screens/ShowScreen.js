dba4e6769889f85f7647b8d2b85ca8c561ba2a98
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-16 22:08:52 +0300

    feat: Add icon pencil in header which will navigate to edit screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Ionicons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
@@ -11,8 +12,25 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 };
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <Pressable onPress={() => navigation.navigate('Edit')} style={styles.editIcon}>
+        <Ionicons name="pencil" size={25} />
+      </Pressable>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  editIcon: {
+    paddingRight: 10,
+  },
+});
 
 export default ShowScreen;
