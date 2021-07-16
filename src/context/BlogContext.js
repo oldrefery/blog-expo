6666da5d2c79ca2963109fd6bead93e68c598e44
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-16 21:41:08 +0300

    feat: Add possibility to add new blog post in common context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,7 +6,8 @@ const blogReducer = (state, action) => {
       return [
         ...state,
         {
-          title: `Blog Post #${state.length + 1}`,
+          title: action.payload.title,
+          content: action.payload.content,
           id: Math.floor(Math.random() * 99999),
         },
       ];
@@ -18,8 +19,8 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: 'add_blogpost' });
+  return (title, content) => {
+    dispatch({ type: 'add_blogpost', payload: { title, content } });
   };
 };
 
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Context } from '../context/BlogContext';
 
-const CreateScreen = () => {
+const CreateScreen = ({ navigation }) => {
+  const { addBlogPost } = useContext(Context);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleClickAdd = () => {
+    addBlogPost(title, content);
+    navigation.pop();
+  };
+
   return (
-    <View>
-      <Text>Create screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput value={title} onChangeText={(newText) => setTitle(newText)} style={styles.input} />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        value={content}
+        onChangeText={(newText) => setContent(newText)}
+        style={styles.input}
+      />
+      <Button title={'Add Blog Post'} onPress={handleClickAdd} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    marginBottom: 15,
+    padding: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+});
 
 export default CreateScreen;
