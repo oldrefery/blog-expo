5339ac67c8d1de72280e6d53d615b8c4a2cefcbb
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-16 18:13:45 +0300

    feat: Add scaffolds for Show Screen and Edit Screen and add them into the stack

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const CreateScreen = () => {
+  return (
+    <View>
+      <Text>Create screen</Text>
+    </View>
+  );
+};
+
+export default CreateScreen;
